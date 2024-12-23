1605778217f34ac6f1a717b8451561b4e7228628
Sanjaykumart <sanjaykumarsanjaykumar8286@gmail.com>
2024-12-23 20:49:01 +0530

    new push

---
+++ script_Sheet/view.js
@@ -0,0 +1,15 @@
+/* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true     
+})
+
+/* -- HOME -- */
+sr.reveal('.featured-text-card',{})
+sr.reveal('.featured-name',{delay: 100})
+sr.reveal('.featured-text-info',{delay: 200})
+sr.reveal('.featured-text-btn',{delay: 200})
+sr.reveal('.social_icons',{delay: 200})
+sr.reveal('.featured-image',{delay: 300})
