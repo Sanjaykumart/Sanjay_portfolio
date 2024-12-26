47ff0e6a197d98a1ff1c98335108b2160a9c42ac
Sanjaykumart <sanjaykumarsanjaykumar8286@gmail.com>
2024-12-26 20:33:05 +0530

    new

--- script_Sheet/view.js
+++ script_Sheet/view.js
@@ -9,14 +9,26 @@ function downloadPDF() {
 }
 document.getElementById("downloadButton").addEventListener("click", downloadPDF);
 
+/* --- ## -- Mail Send Code -- ## --- */
+emailjs.init("your_public_key");
+document.getElementById("contactForm").addEventListener("submit", function (event) {
+    event.preventDefault();
+    emailjs.sendForm("your_service_id", "your_template_id", this)
+        .then(function (response) {
+            alert("Email sent successfully!");
+        }, function (error) {
+            alert("Failed to send email. Please try again.");
+        });
+});
+
 
 /* ----- ## -- SCROLL REVEAL ANIMATION -- ## ----- */
 const scrollReveal = ScrollReveal({
     origin: 'top',
     distance: '80px',
     duration: 2000,
-    reset: true     
+    reset: true
 })
 
 /* -- HOME -- */
-scrollReveal.reveal('.featured-image',{delay: 300})
+scrollReveal.reveal('.featured-image', { delay: 300 })
